8bdf7dee58ac4843aaeb741c91ca306e5c394327
syama98 <s.yamamoto.ad98@gmail.com>
2022-07-02 16:54:59 +0900

    夕飯候補の要素を取得
    script.js内に関数getElementsを定義
    index.html内にてボタンをクリックした時に要素を取得するよう変更

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function getElement() {
     element = document.querySelector('#shopB');
     console.log("選ばれたのは" + element.textContent + "でした");
 }
+
+function getElements() {
+    elements = document.querySelectorAll('.shop li');
+    let len = elements.length;
+    for (let i = 0; i < len; i++) {
+        console.log(elements.item(i).textContent);
+    }
+}
