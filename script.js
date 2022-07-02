d501de423f4e77f968fe66fad1f2b39ad0afde0d
syama98 <s.yamamoto.ad98@gmail.com>
2022-07-02 17:22:37 +0900

    コンソールでイベントの確認

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-function getElement() {
-    element = document.querySelector('#shopB');
-    console.log("選ばれたのは" + element.textContent + "でした");
-}
-
-function getElements() {
-    elements = document.querySelectorAll('.shop li');
-    let len = elements.length;
-    for (let i = 0; i < len; i++) {
-        console.log(elements.item(i).textContent);
-    }
-}
+let lists = document.getElementsByClassName("pull-down-list");
+console.log(lists);
