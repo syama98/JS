2c867d2ded04fc83dff62a697f8f062e6718b6e6
syama98 <s.yamamoto.ad98@gmail.com>
2022-07-06 15:59:48 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const pullDownButton = document.getElementById("lists");
+const pullDownParents = document.getElementById("pull-down");
 
 window.addEventListener('load', function(){
     const pullDownButton = document.getElementById("lists");
 })
 
 pullDownButton.addEventListener('mouseover', function(){
-    pullDownButton.setAttribute("style", "background-color: blue;");
+    this.setAttribute("style", "background-color: blue;");
 })
 pullDownButton.addEventListener('click', function(){
-    pullDownButton.setAttribute("style", "background-color: green;");
+    if (pullDownParents.getAttribute("style") == "display: block;") {
+        pullDownParents.removeAttribute("style", "display: block;");
+    } else {
+        pullDownParents.setAttribute("style", "display: block;");
+    }
 })
 pullDownButton.addEventListener('mouseout', function(){
-    pullDownButton.removeAttribute("style");
+    this.removeAttribute("style");
 })
